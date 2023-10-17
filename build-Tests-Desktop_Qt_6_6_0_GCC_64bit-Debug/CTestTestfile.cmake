ea146d0113b9b7ac5c4af7f61aff54bc0ed0d918
Maksym Klymenko <maksym@pop-os.localdomain>
2023-10-17 20:06:44 +0300

    gitignore 3rd try

--- build-Tests-Desktop_Qt_6_6_0_GCC_64bit-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maksym/uni4/IT/DBMS/Tests
-# Build directory: /home/maksym/uni4/IT/DBMS/build-Tests-Desktop_Qt_6_6_0_GCC_64bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DBMS "/home/maksym/uni4/IT/DBMS/build-Tests-Desktop_Qt_6_6_0_GCC_64bit-Debug/DBMS")
-set_tests_properties(DBMS PROPERTIES  _BACKTRACE_TRIPLES "/home/maksym/uni4/IT/DBMS/Tests/CMakeLists.txt;23;add_test;/home/maksym/uni4/IT/DBMS/Tests/CMakeLists.txt;0;")
